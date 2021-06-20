dbe05eea96e15b0197dc34b706520f6a4cc6bbb8
marz-20 <170320116057.it.mayank@gmail.com>
2021-06-20 11:20:18 +0530

    footer updated

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -2,7 +2,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                   &copy; Online Library Management System |<a  target="_blank"  > Designed by : Zainul</a> 
+                   &copy; Online Library Management System |<a> Designed by : Zainul</a> 
                 </div>
 
             </div>

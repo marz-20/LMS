b412c70d04ac4dcf2441cf89228997dc7a210092
marz-20 <170320116057.it.mayank@gmail.com>
2021-06-20 10:45:48 +0530

    final commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; Online Library Management System |<a  target="_blank"  > Designed by : Zainul</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
